f92ebe1a4a72b91b17cc26b4301e944e5f488935
AnshVM <anshmalik2002@gmail.com>
2021-09-29 11:46:12 +0530

    Hash passwords

--- controllers/User.js
+++ controllers/User.js
@@ -3,26 +3,30 @@ const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res) => {
     const { username, email, password } = req.body;
-    console.log("recived req")
-    const user = new User({
-        username,
-        email,
-        password,
-        followers: [],
-        following: [],
-        posts: [],
-        joinedAt: new Date(),
-    })
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+            const user = new User({
+                username,
+                email,
+                password: hash,
+                followers: [],
+                following: [],
+                posts: [],
+                joinedAt: new Date(),
+            })
 
-    user.save()
-        .then((user) => {
-            console.log(`New user: ${JSON.stringify(user)}`);
-            return res.status(201).json(user);
-        })
-        .catch((err) => {
-            if (err.errors.username)
-                return res.status(400).json(`Account with this username already exists`)
-            if (err.errors.email)
-                return res.status(400).json(`Account with this email already exists`)
-        })
+            user.save()
+                .then((user) => {
+                    return res.status(201).json("Success");
+                })
+                .catch((err) => {
+                    if (err.errors.username)
+                        return res.status(400).json(`Account with this username already exists`)
+                    if (err.errors.email)
+                        return res.status(400).json(`Account with this email already exists`)
+                })
+        });
+    });
 }
+
+
